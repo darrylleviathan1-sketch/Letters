b94faa1550fddf34379b8b76d459b1f1d32c6a06
darrylleviathan1-sketch <darrylleviathan1@gmail.com>
2026-07-30 00:07:11 +0700

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -57,15 +57,6 @@ let teks = "I will always wish the best things to you, and i hope that we will a
 
 let index = 0;
 
- function ketik(){
-
-    if(index < teks.length){
-
-        document.getElementById("surat").innerHTML += teks.charAt(index);
-
-        index++;
-
-        setTimeout(ketik,30);
 
     }
 
